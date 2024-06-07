0c84056e89b342b510463ab1dff3e1974382523d
EnjunCai <1208823155@qq.com>
2024-06-07 21:23:40 +0800

    fix:打包问题修复

--- src/directives/scrollReveal.ts
+++ src/directives/scrollReveal.ts
@@ -3,24 +3,26 @@ import { DirectiveBinding } from "vue";
 const activeIntersection = new IntersectionObserver((entries, observer) => {
   entries.forEach((entry) => {
     let target = entry.target as HTMLElement;
-    console.log(target);
-    entry.target.style.opacity = '0';
-    entry.target.style.transition = 'all .8s';
-    
-    if (entry.isIntersecting) {
+
+    if (entry.target instanceof HTMLElement) {
+      console.log(target);
+      entry.target.style.opacity = "0";
+      entry.target.style.transition = "all .8s";
+
+      if (entry.isIntersecting) {
         // 监听到元素在视窗内，将元素style的opacity设置为1
-        entry.target.style.opacity = '1';
-        
+        entry.target.style.opacity = "1";
+
         console.log(entry.target);
-        
-      } 
+      }
+    }
   });
 });
 
 export const scrollReveal = {
   mounted(el: HTMLElement, binding: DirectiveBinding) {
-        console.log(el,binding);
-        
+    console.log(el, binding);
+
     // if (binding.value.move == "right") {
     //   el.classList.add("defaultObserveRight");
     // } else if (binding.value.move == "bottom") {
