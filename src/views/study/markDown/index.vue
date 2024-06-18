63e8d77fc33b76f9e58f47df0cd47cf06e73125f
intstring <intstring@163.com>
2024-06-18 11:21:41 +0800

    fix：解决图片显示问题

--- src/views/study/markDown/index.vue
+++ src/views/study/markDown/index.vue
@@ -11,7 +11,7 @@
 
     <MdEditor v-model="text" />
 
-    <MdPreview :editorId="id" :modelValue="text1" />
+    <MdPreview :editorId="id" :modelValue="text" />
   </div>
   <!-- <MdCatalog :editorId="id1" :scrollElement="scrollElement" /> -->
 </template>
@@ -28,10 +28,30 @@ const id = "preview-only";
 const id1 = "preview-only1";
 const text1 = ref("# Hello Editor");
 const scrollElement = document.documentElement;
-
+const infoUrl = ref();
+infoUrl.value = new URL("@/assets/images/kaifa.svg", import.meta.url).pathname;
 const text = ref(`# Hello Editor
 
-![图片](/src/assets/images/kaifa.svg)`);
+![图片](${infoUrl.value})`);
+
+// # 通过vercel发布静态页面
+// 前提：需要一个域名
+
+// 进入[vercel](https://vercel.com/)
+
+// 登录绑定你项目
+// t1
+
+// 进入后点击Project绑定项目
+// t2
+
+// 里面选择你需要发布的项目
+// t3
+
+// 导入成功后会在你的主界面看到这个项目，到这里已经发布上去，但是国内无法访问，需要进行额外配置
+// t4
+
+// ![图片](/src/assets/images/kaifa.svg)
 
 watch(
   () => text.value,
